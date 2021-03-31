51228802a32a7c18dd5cc3edca7ef0f6c41589a7
Chakrapani Gautam <cherrygot@DESKTOP-VL7NGRV.localdomain>
2021-03-31 17:20:50 +0530

    Added a simple server for landing page

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello from index.js");
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080;
+const staticDir = ['public', 'dist'];
+
+const server = http.createServer((req, res) => {
+  if (!shouldServeStaticOn(req.url)) {
+    req.url = '/public/index.html';
+  }
+
+  respondWithStaticFile(req, res);
+});
+
+function shouldServeStaticOn(url) {
+  return staticDir.some((dir) => {
+    return new RegExp('^/' + dir).test(url);
+  });
+}
+
+function respondWithStaticFile(req, res) {
+  fs.readFile(__dirname + req.url, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+
+      return;
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+server.listen(port);
